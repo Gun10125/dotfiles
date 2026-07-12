35b79b97bfbd9f3b8134bebd250928072f8a0ff8
Gun10125 <bhingkarawatn@gmail.com>
2026-07-12 10:55:29 +0700

    initial dotfiles setup

---
+++ powershell/profile.ps1
@@ -0,0 +1,30 @@
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+
+function gs { git status }
+function ga { git add . }
+function gc { param($msg) git commit -m $msg }
+function gp { git push }
+
+# Oh My Posh prompt theme
+oh-my-posh init pwsh --config "$HOME\Documents\dotfiles\powershell\my_profile.omp.json" | Invoke-Expression
+
+# Terminal Icon
+Import-Module -Name Terminal-Icons
+
+# Directory jumper
+Import-Module -Name z
+
+# History & Autocomplete
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Utility: find full path of a command
+Set-Alias w whereis
+function whereis {
+    param([string]$name)
+    Get-Command $name -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Source
+}
